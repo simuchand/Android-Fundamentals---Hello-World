d9c402557ab1f223a30b1553b92e14e01b09bdfb
Simuchand <simuchand.balachandran@gmail.com>
2020-03-19 12:58:54 +0530

    Hello World

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Log.d("MainActivity", "Hello World");
+        //Log.e("MainActivity", "Hello World");
+        Log.i("MainActivity", "MainActivity layout is complete");
+    }
+}
